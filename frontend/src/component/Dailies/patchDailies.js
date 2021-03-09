afa4a63796c19c073766edbbb476d0798cfcb8a5
vanStaen <clement.vanstaen@gmail.com>
2021-03-09 13:08:02 +0100

    patch dailies

---
+++ frontend/src/component/Dailies/patchDailies.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const getDailies = async (activity, value) => {
+
+    const today = new Date();
+
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+
+    const requestBody = {
+        activity: value
+    };
+
+    console.log(requestBody);
+
+    const response = await axios({
+        url: process.env.REACT_APP_API_URL + `dailies/${year}/${month}/${day}`,
+        method: "PATCH",
+        data: requestBody,
+    });
+    if ((response.status !== 200) & (response.status !== 201)) {
+        throw new Error("Error!");
+    }
+
+    return response.status;
+}
