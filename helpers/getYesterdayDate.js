756dcb277abfca17f0c762d4df479667f59c9167
Clément van Staen <clement.vanstaen@gmail.com>
2021-03-10 18:11:00 +0100

    fixing bugs

--- helpers/getYesterdayDate.js
+++ helpers/getYesterdayDate.js
@@ -1,7 +1,7 @@
-export function getYesterdayDate(toYear, toMonth, toDay) {
+function getYesterdayDate(toYear, toMonth, toDay) {
 
-    let today = new Date(toYear, toMonth-1 , toDay, 0, 0, 0, 0);
-    var yesterday = new Date(today.valueOf() - 1000*60*60*24);
+    const today = new Date(toYear, toMonth-1 , toDay, 0, 0, 0, 0);
+    const yesterday = new Date(today.valueOf() - 1000*60*60*24);
 
     const yesterYear = yesterday.getFullYear();
     const yesterMonth = yesterday.getMonth() +1;
@@ -9,3 +9,13 @@ export function getYesterdayDate(toYear, toMonth, toDay) {
 
     return [yesterYear, yesterMonth, yesterDay];
 }
+
+/*
+// tests
+console.log(getYesterdayDate(2020,03,08));
+console.log(getYesterdayDate(2020,04,01));
+console.log(getYesterdayDate(2020,09,01));
+console.log(getYesterdayDate(2020,1,01));
+*/
+
+exports.getYesterdayDate = getYesterdayDate;
