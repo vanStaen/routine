9cc7266484285f826ba40f01a0ba7e6d241e93d6
vanStaen <clement.vanstaen@gmail.com>
2021-03-10 14:46:45 +0100

    streak endpoint WIP

---
+++ api/streack.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const { Client } = require("pg");
+
+
+// Init Postgres
+const client = new Client({ connectionString: process.env.DATABASE_URL, ssl: true })
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; // This bypasses the SSL verification
+
+// Connect to Postgres 
+client.connect(err => {
+    if (err) {
+        console.error('connection error', err.stack)
+    } else {
+        console.log('Activity API:', 'Connected to postgres db!')
+    }
+})
+
+// GET  streak data for all activities
+router.get("/", async (req, res) => {
+    try {
+        const activities = await client.query('SELECT * FROM streak;');
+        res.status(201).json(activities.rows);
+    } catch (err) {
+        res.status(400).json({
+            error: `${err})`,
+        });
+    }
+});
+
+
+// GET streak data for specific activity
+router.get("/:activity", async (req, res) => {
+    try {
+        const activities = await client.query(`SELECT * FROM streak WHERE '${req.params.activity}'`);
+        res.status(201).json(activities.rows);
+    } catch (err) {
+        res.status(400).json({
+            error: `${err})`,
+        });
+    }
+});
