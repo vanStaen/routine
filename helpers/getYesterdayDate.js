7363618afe0b93940c5200d7135e4d93af4767fb
Clément van Staen <clement.vanstaen@gmail.com>
2021-03-10 21:55:28 +0100

    streak endpoint done

--- helpers/getYesterdayDate.js
+++ helpers/getYesterdayDate.js
@@ -1,5 +1,4 @@
-function getYesterdayDate(toYear, toMonth, toDay) {
-
+module.exports =  function getYesterdayDate(toYear, toMonth, toDay) {
     const today = new Date(toYear, toMonth-1 , toDay, 0, 0, 0, 0);
     const yesterday = new Date(today.valueOf() - 1000*60*60*24);
 
@@ -17,5 +16,3 @@ console.log(getYesterdayDate(2020,04,01));
 console.log(getYesterdayDate(2020,09,01));
 console.log(getYesterdayDate(2020,1,01));
 */
-
-exports.getYesterdayDate = getYesterdayDate;
