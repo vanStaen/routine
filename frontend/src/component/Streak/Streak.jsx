880179babf0e6eb92ba8fcbeb3905690e02ad9c7
Clément van Staen <clement.vanstaen@gmail.com>
2021-03-12 10:58:46 +0100

    streak color

--- frontend/src/component/Streak/Streak.jsx
+++ frontend/src/component/Streak/Streak.jsx
@@ -21,10 +21,49 @@ export const Streak = (props) => {
     fetchStreak();
   }, []);
 
+  const backGroundColor = (value) => {
+    if (value < 2) {
+      return 0.1;
+    } else if (value < 3) {
+      return 0.2;
+    } else if (value < 5) {
+      return 0.3;
+    } else if (value < 8) {
+      return 0.4;
+    } else if (value < 15) {
+      return 0.5;
+    } else if (value < 30) {
+      return 0.55;
+    } else if (value < 55) {
+      return 0.65;
+    } else if (value < 90) {
+      return 0.7;
+    } else if (value < 120) {
+      return 0.75;
+    } else if (value < 180) {
+      return 0.8;
+    } else if (value < 250) {
+      return 0.85;
+    } else if (value < 350) {
+      return 0.9;
+    } else {
+      return 1;
+    }
+  };
+
   return isLoading ? (
     <div className="Streak__Main">?</div>
   ) : streak[activity] ? (
-    <div className="Streak__Main">{streak[activity]}</div>
+    <div
+      className="Streak__Main"
+      style={{
+        backgroundColor: `rgba(214, 137, 16, ${backGroundColor(
+          streak[activity]
+        )})`,
+      }}
+    >
+      {streak[activity]}
+    </div>
   ) : (
     <></>
   );
