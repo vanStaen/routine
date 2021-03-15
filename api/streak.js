ee6afcfe7a6bd47e098f8c202b78d4b7cda9d77c
vanStaen <clement.vanstaen@gmail.com>
2021-03-15 12:00:02 +0100

    update streak endpoint

--- api/streak.js
+++ api/streak.js
@@ -81,6 +81,88 @@ router.get("/:year/:month/:day", async (req, res) => {
     }
 });
 
+
+// PATCH single data from streak for specific date
+router.patch("/:year/:month/:day", async (req, res) => {
+    let updateField = '';
+    if (req.body.day !== undefined) {
+        updateField = updateField + "day=" + req.body.day + ",";
+    }
+    if (req.body.month !== undefined) {
+        updateField = updateField + "month=" + req.body.month + ",";
+    }
+    if (req.body.year !== undefined) {
+        updateField = updateField + "year=" + req.body.year + ",";
+    }
+    if (req.body.dutch !== undefined) {
+        updateField = updateField + "dutch=" + req.body.dutch + ",";
+    }
+    if (req.body.guitar !== undefined) {
+        updateField = updateField + "guitar=" + req.body.guitar + ",";
+    }
+    if (req.body.pushups !== undefined) {
+        updateField = updateField + "pushups=" + req.body.pushups + ",";
+    }
+    if (req.body.pullups !== undefined) {
+        updateField = updateField + "pullups=" + req.body.pullups + ",";
+    }
+    if (req.body.situps !== undefined) {
+        updateField = updateField + "situps=" + req.body.situps + ",";
+    }
+    if (req.body.javascript !== undefined) {
+        updateField = updateField + "javascript=" + req.body.javascript + ",";
+    }
+    if (req.body.teeth !== undefined) {
+        updateField = updateField + "teeth=" + req.body.teeth + ",";
+    }
+    if (req.body.producing !== undefined) {
+        updateField = updateField + "producing=" + req.body.producing + ",";
+    }
+    if (req.body.bass !== undefined) {
+        updateField = updateField + "bass=" + req.body.bass + ",";
+    }
+    if (req.body.piano !== undefined) {
+        updateField = updateField + "piano=" + req.body.piano + ",";
+    }
+    if (req.body.trumpet !== undefined) {
+        updateField = updateField + "trumpet=" + req.body.trumpet + ",";
+    }
+    if (req.body.run !== undefined) {
+        updateField = updateField + "run=" + req.body.run + ",";
+    }
+    if (req.body.stretch !== undefined) {
+        updateField = updateField + "stretch=" + req.body.stretch + ",";
+    }
+    if (req.body.photo !== undefined) {
+        updateField = updateField + "photo=" + req.body.photo + ",";
+    }
+    if (req.body.water !== undefined) {
+        updateField = updateField + "water=" + req.body.water + ",";
+    }
+    if (req.body.climb !== undefined) {
+        updateField = updateField + "climb=" + req.body.climb + ",";
+    }
+    const updateFieldEdited = updateField.slice(0, -1) // delete the last comma
+    const updateQuery = `UPDATE streak SET ${updateFieldEdited}  WHERE year=${req.params.year} AND month=${req.params.month} AND day=${req.params.day}`;
+    try {
+        const udpate = await client.query(updateQuery);
+        if (udpate.rowCount > 0) {
+            res.status(200).json({
+                success: `Streak updated.`,
+            });
+        } else {
+            res.status(400).json({
+                error: `No streak found for the ${eq.params.day}/${eq.params.month}/${eq.params.year}.`,
+            });
+        }
+    } catch (err) {
+        res.status(400).json({
+            error: `${err}`,
+        });
+    }
+});
+
+
 // Function to update the Streak table
 const updateStreakBasedonYesterday = async (toYear, toMonth, toDay) => {
     try {
