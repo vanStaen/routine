582a727ba486e37840227479bfcdc8678805ec23
Clément van Staen <clement.vanstaen@gmail.com>
2021-03-18 13:09:22 +0100

    adapt var VH on resize

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import { Dailies } from './component/Dailies/Dailies';
 
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 function App() {
   return (
     <div className="App">
