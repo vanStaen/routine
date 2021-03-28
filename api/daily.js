5d1f0f7cb418987b6644e1580840e371920ccf93
vanStaen <clement.vanstaen@gmail.com>
2021-03-28 17:35:06 +0200

    dynamisches patch daily

--- api/daily.js
+++ api/daily.js
@@ -44,62 +44,8 @@ router.get("/:year/:month/:day", async (req, res) => {
 // PATCH single data from daily (based on date)
 router.patch("/:id", async (req, res) => {
   let updateField = '';
-  if (req.body.day !== undefined) {
-    updateField = updateField + "day=" + req.body.day + ",";
-  }
-  if (req.body.month !== undefined) {
-    updateField = updateField + "month=" + req.body.month + ",";
-  }
-  if (req.body.year !== undefined) {
-    updateField = updateField + "year=" + req.body.year + ",";
-  }
-  if (req.body.dutch !== undefined) {
-    updateField = updateField + "dutch=" + req.body.dutch + ",";
-  }
-  if (req.body.guitar !== undefined) {
-    updateField = updateField + "guitar=" + req.body.guitar + ",";
-  }
-  if (req.body.pushups !== undefined) {
-    updateField = updateField + "pushups=" + req.body.pushups + ",";
-  }
-  if (req.body.pullups !== undefined) {
-    updateField = updateField + "pullups=" + req.body.pullups + ",";
-  }
-  if (req.body.situps !== undefined) {
-    updateField = updateField + "situps=" + req.body.situps + ",";
-  }
-  if (req.body.javascript !== undefined) {
-    updateField = updateField + "javascript=" + req.body.javascript + ",";
-  }
-  if (req.body.teeth !== undefined) {
-    updateField = updateField + "teeth=" + req.body.teeth + ",";
-  }
-  if (req.body.producing !== undefined) {
-    updateField = updateField + "producing=" + req.body.producing + ",";
-  }
-  if (req.body.bass !== undefined) {
-    updateField = updateField + "bass=" + req.body.bass + ",";
-  }
-  if (req.body.piano !== undefined) {
-    updateField = updateField + "piano=" + req.body.piano + ",";
-  }
-  if (req.body.trumpet !== undefined) {
-    updateField = updateField + "trumpet=" + req.body.trumpet + ",";
-  }
-  if (req.body.run !== undefined) {
-    updateField = updateField + "run=" + req.body.run + ",";
-  }
-  if (req.body.stretch !== undefined) {
-    updateField = updateField + "stretch=" + req.body.stretch + ",";
-  }
-  if (req.body.photo !== undefined) {
-    updateField = updateField + "photo=" + req.body.photo + ",";
-  }
-  if (req.body.water !== undefined) {
-    updateField = updateField + "water=" + req.body.water + ",";
-  }
-  if (req.body.climb !== undefined) {
-    updateField = updateField + "climb=" + req.body.climb + ",";
+  for (const [key, value] of Object.entries(req.body)) {
+    updateField = updateField + `${key}=${value},`;
   }
   const updateFieldEdited = updateField.slice(0, -1) // delete the last comma
   const updateQuery = `UPDATE dailies SET ${updateFieldEdited} WHERE id=${req.params.id}`;
