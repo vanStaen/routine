4a1fc6c3c04eb72e244e4579005adb9dbd4d312b
Clément van Staen <clement.vanstaen@gmail.com>
2021-04-07 15:37:23 +0200

    name in custom spinner

--- frontend/src/component/Spinner/Spinner.jsx
+++ frontend/src/component/Spinner/Spinner.jsx
@@ -1,8 +1,14 @@
 import logoVenja from "../../images/venja.svg";
-import './Spinner.css';
+import "./Spinner.css";
 
 export const Spinner = () => {
-    return (<div className="spinner">
-        <img src={logoVenja} className="loader" alt="Loading" />
-    </div>)
-}   
+  return (
+    <div className="spinner">
+      <img src={logoVenja} className="loader" alt="Loading" />
+      <div className="spinner spinner__header">
+        venja
+        <span style={{ color: "#D68910" }}>.org</span>
+      </div>
+    </div>
+  );
+};
