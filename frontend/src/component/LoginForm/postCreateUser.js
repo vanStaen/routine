82613dc8120f3baef5a00f66476e884a8d3022f5
vanStaen <clement.vanstaen@gmail.com>
2021-04-20 08:25:18 +0200

    Add Auth to Routine

--- frontend/src/component/LoginForm/postCreateUser.js
+++ frontend/src/component/LoginForm/postCreateUser.js
@@ -4,22 +4,22 @@ import axios from "axios";
 
 export const postCreateUser = async (email, password) => {
 
-    let requestBody = { email: email, password: password };
+  let requestBody = { email: email, password: password };
 
-    const response = await axios(process.env.REACT_APP_AUTH_URL, {
-      method: "POST",
-      body: JSON.stringify(requestBody),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  const response = await axios(process.env.REACT_APP_API_URL, {
+    method: "POST",
+    body: JSON.stringify(requestBody),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if ((response.status !== 200) & (response.status !== 201)) {
-      const error = response.json();
-      const message = `An error has occured: ${response.status} - ${error.errors[0].message}`;
-      console.log("Error!", message);
-    }
-
-    const login = response.json();
-    return login;
+  if ((response.status !== 200) & (response.status !== 201)) {
+    const error = response.json();
+    const message = `An error has occured: ${response.status} - ${error.errors[0].message}`;
+    console.log("Error!", message);
   }
+
+  const login = response.json();
+  return login;
+}
