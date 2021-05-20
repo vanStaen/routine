eb56d53f535bb7cafab7ce9067405ad7949f5264
vanStaen <clement.vanstaen@gmail.com>
2021-05-20 19:19:22 +0200

    obstacle endpoint

---
+++ api/obstacle.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const moment = require("moment-timezone");
+const router = express.Router();
+const { Client } = require("pg");
+
+// Init Postgres
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; // This bypasses the SSL verification
+
+// Connect to Postgres
+client.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  }
+});
+
+// GET obstacle data specific user
+router.get("/", async (req, res) => {
+  const year = moment().tz("Europe/Berlin").format("YYYY");
+  const month = moment().tz("Europe/Berlin").format("MM");
+  const day = moment().tz("Europe/Berlin").format("DD");
+  if (!req.isAuth) {
+    res.status(401).json({
+      error: "Unauthorized",
+    });
+    return;
+  }
+  try {
+    const streak = await client.query(
+      `SELECT * FROM obstacle WHERE year=${year} AND month=${month} AND day=${day} AND userid='${req.userId}'`
+    );
+    res.status(200).json(streak.rows);
+  } catch (err) {
+    res.status(400).json({
+      error: `${err})`,
+    });
+  }
+});
+
+// GET obstacle data for specific date, and specific user
+router.get("/:year/:month/:day", async (req, res) => {
+  if (!req.isAuth) {
+    res.status(401).json({
+      error: "Unauthorized",
+    });
+    return;
+  }
+  try {
+    const streak = await client.query(
+      `SELECT * FROM obstacle WHERE year=${req.params.year} AND month=${req.params.month} AND day=${req.params.day} AND userid='${req.userId}'`
+    );
+    res.status(200).json(streak.rows);
+  } catch (err) {
+    res.status(400).json({
+      error: `${err})`,
+    });
+  }
+});
+
+
+// DELETE obstacle from table
+router.delete("/:id", async (req, res) => {
+  try {
+    const deleteObstacle = `DELETE FROM obstacle WHERE id='${req.params.id}'`;
+    await client.query(deleteObstacle);
+    res.status(201).json({ success: `Obastacle with id #${req.params.id} was deleted.` });
+  } catch (err) {
+    res.status(400).json({
+      error: `${err})`,
+    });
+  }
+});
+
+module.exports = router;
